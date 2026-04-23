f07c573ed4525ef0e8447829d0895b26b1e84a30
amitesh56 <yadavamitesh44@gmail.com>
2026-04-23 15:13:40 +0000

    started the new blog api backend

---
+++ src/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("DB is connected");
+
+        
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
