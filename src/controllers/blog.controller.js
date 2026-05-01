8e4a87c9e4ccd8a4d398669bb27c132bff9b3aab
amitesh56 <21301071amitesh@viva-technology.org>
2026-05-01 14:02:44 +0530

    complited the blog controller CRUD operation on blog

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -51,6 +51,119 @@ async function createBlog(req,res) {
 
 }
 
+async function getSingleBlog(req,res) {
+    try {
+        const blogId = req.params.id ;
+        const isBlogexisist = await blogModel.findById(blogId)
+        if(!isBlogexisist){
+            return res.status(404).json({
+                message : "This blog does not exisist"
+            })
+        }
+        res.status(200).json({
+            isBlogexisist
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            message : error.message
+        })
+    }
+}
 
+async function getAllBlog(req,res) {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+        const blog = await blogModel.find({}).sort({ createdAt: -1 }).skip(skip).limit(limit);
+        res.status(200).json({
+            
+            page,
+            limit,
+            count: blogs.length,
+            blogs
+           
+        })
+        
+        
+    } catch (error) {
+        res.status(500).json({
+            message : error.message
+        })
+    }
+}
 
-module.exports = {createBlog}
+async function updateBlog(req, res) {
+    try {
+        const blogId = req.params.id;
+        const owner = req.user.id;
+
+        const blogData = verifyData.safeParse(req.body);
+
+        if (!blogData.success) {
+            return res.status(422).json({
+                message: blogData.error.errors.map(e => e.message)
+            });
+        }
+
+        const { title, content } = blogData.data;
+
+        const updateFields = { title, content };
+
+        if (req.file) {
+            updateFields.coverImage = await imageUpload(req.file);
+        }
+
+        const updatedBlog = await blogModel.findOneAndUpdate(
+            { _id: blogId, author: owner },
+            updateFields,
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedBlog) {
+            return res.status(404).json({
+                message: "Blog not found or not authorized"
+            });
+        }
+
+        res.status(200).json({
+            message: "Blog updated successfully",
+            blog: updatedBlog
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+async function deleteBlog(req, res) {
+    try {
+        const blogId = req.params.id;
+        const owner = req.user.id;
+
+        const deletedBlog = await blogModel.findOneAndDelete({
+            _id: blogId,
+            author: owner
+        });
+
+        if (!deletedBlog) {
+            return res.status(404).json({
+                message: "Blog not found or not authorized"
+            });
+        }
+
+        res.status(200).json({
+            message: "Blog deleted successfully"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+module.exports = {createBlog , getSingleBlog , getAllBlog , updateBlog, deleteBlog}
